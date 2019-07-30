e05d1553be50e882f45facb5647880c5702cf8c3
김도영 <rlaehdud4992@ssu.ac.kr>
2019-07-30 15:54:17 +0900

    목표 : 이중 연결 리스트(Double LinkedList)를 이용하여 큐 구현하기  => 선입선출(First In Fist Out) 구조를 띄는 자료구조 구현 1. queue.h : 추상자료형(ADT) 명세 2. boolean.h : 프로젝트 구현 편의를 위해 TRUE/FALSE를 정의
    테스팅까지 모두 완료

--- queue.c
+++ queue.c
@@ -3,11 +3,32 @@
 #include <stdlib.h>
 #include "queue.h"
 #include "boolean.h"
+/*
+    목표 : 연결리스트(LinkedList)를 이용하여 큐를 구현
+    queue.h : 추상자료형(ADT) 명세
+    boolean.h : 프로젝트 구현 편의를 위해 TRUE / FALSE를 정의
+    선입선출(FIFO) 구조를 띈다.
+ */
 
 int main() {
     Queue* queue;
     queue = createQueue(queue);
     printf("큐 생성 완료\n");
+
+    queue = addNode(queue, 5);
+    queue = addNode(queue, 4);
+    queue = addNode(queue, 3);
+
+    peek(queue);
+
+    removeNode(queue);
+    peek(queue);
+
+    removeNode(queue);
+    peek(queue);
+
+    removeNode(queue);
+    peek(queue);
 
     if(deleteQueue(queue)) {
         printf("큐 메모리 반환 완료\n");
@@ -15,12 +36,16 @@ int main() {
     else {
         printf("큐 메모리 반환 실패..\n");
     }
+
+    return 0;
 }
 
 
 // 큐 생성 : front, rear 노드를 생성하고 노드를 초기화
 Queue* createQueue(Queue* queue) {
+    // 큐 메모리 할당
     queue = (Queue*)malloc(sizeof(Queue));
+
     // 큐에 메모리 할당 실패 시
     if(queue == NULL) {
         printf("queue에 메모리 할당 실패\n");
@@ -28,6 +53,7 @@ Queue* createQueue(Queue* queue) {
     }
 
     queue->front = (queueData*)malloc(sizeof(queueData));
+
     // 가장 처음 노드에 메모리 할당 실패 시
     if(queue->front == NULL) {
         printf("queue->front에 메모리 할당 실패\n");
@@ -38,6 +64,7 @@ Queue* createQueue(Queue* queue) {
     }
 
     queue->rear = (queueData*)malloc(sizeof(queueData));
+
     // 마지막 노드에 메모리 할당 실패 시
     if(queue->rear == NULL) {
         printf("queue->rear에 메모리 할당 실패\n");
@@ -51,23 +78,40 @@ Queue* createQueue(Queue* queue) {
         return NULL;
     }
 
-    // 큐 초기화
+    // 노드 초기화
     queue->current = queue->rear;
-    queue->front->next = NULL;
+
+    // 처음(front) 노드 설정
+    queue->front->previous = NULL;
+    queue->front->next = queue->rear;
+
+    // 마지막(rear) 노드 설정
     queue->rear->next = NULL;
+    queue->rear->previous = queue->front;
+
     return queue;
 }
 
+
 // 큐 제거 : 동적 할당한 모든 노드의 메모리를 반환
 _Bool deleteQueue(Queue* queue) {
-    // 마지막 노드 메모리 반환
-    memset(queue->rear, 0xFF, sizeof(queue->rear));
-    free(queue->rear);
-    queue->rear = NULL;
+    // 임시로 데이터를 저장할 노드 메모리 할당
+    struct queueData* temp = (struct queueData*)malloc(sizeof(struct queueData));
 
-    // 처음 노드 메모리 반환
-    memset(queue->front, 0xFF, sizeof(queue->front));
-    free(queue->front);
+    // 현재 가리키는 노드는 처음 노드
+    queue->current = queue->front;
+
+    // 전체 데이터 노드 순차적으로 순회
+    while (queue->current == NULL) {
+        temp = queue->current->next;
+        memset(queue->current, 0xFF, sizeof(queue->current));
+        free(queue->current);
+        queue->current = temp;
+    }
+
+    // 포인터 반환 작업
+    queue->current = NULL;
+    queue->rear = NULL;
     queue->front = NULL;
 
     // 큐(queue) 메모리 반환
@@ -75,44 +119,99 @@ _Bool deleteQueue(Queue* queue) {
     free(queue);
     queue = NULL;
 
+    // 임시 노드 메모리 반환
+    memset(temp, 0xFF, sizeof(temp));
+    free(temp);
+    temp = NULL;
+
     return TRUE;
 }
 
+
 // 큐 추가(enqueue) : 큐의 가장 마지막에 데이터를 추가
-Queue* addNode(Queue* queue, int number) {
-    // 데이터 노드 동적 할당
-    queueData* data = (queueData*)malloc(sizeof(queueData));
+Queue* addNode(Queue* queue, const int number) {
 
-    // 데이터 노드에 데이터 update
-    data->number = number;
+    // 데이터 노드 메모리 할당
+    queueData* newData = (queueData*)malloc(sizeof(queueData));
 
-    // 큐의 현재 노드 위치 초기화
-    queue->current = queue->rear;
-    // 만약 연결된 데이터 노드가 없으면
-    if(queue->rear->next == NULL) {
-        queue->rear->next = data;
-        data->next = queue->front;
-        return queue;
+    // 노드에 매개변수로 받아온 데이터 저장
+    newData->number = number;
+
+    // 연결된 데이터 노드가 존재하지 않는다면
+    if(isEmpty(queue)) {
+        // 처음(front) 노드와 추가할 노드 연결
+        queue->front->next = newData;
+        newData->previous = queue->front;
+
+        // 추가할 노드와 마지막(rear) 노드 연결
+        newData->next = queue->rear;
+        queue->rear->previous = newData;
     }
+    // 기존에 1개 이상의 연결되어 있는 데이터 노드가 존재하면
     else {
-        // 현재 노드가 front(처음) 노드가 되기 전까지 노드 순회
-        while(queue->rear->next->next == NULL) {
-            queue->current = queue->rear->next;
-        }
-        // 새로운 노드가 마지막 노드가 되도록 노드 연결
-        data->next = queue->current->next;
-        queue->current->next = data;
+        // 가장 처음 데이터 노드를 추가할 노드와 연결
+        queue->current = queue->front->next;
+        queue->current->previous = newData;
+
+        // 추가할 노드 설정
+        newData->previous = queue->front;
+        newData->next = queue->current;
+
+        // 처음(front) 노드의 다음(next) 노드는 추가할 노드
+        queue->front->next = newData;
     }
+    return queue;
 }
 
+
 // 큐 제거(dequeue) : 큐의 가장 처음 데이터를 제거
-Queue* removeNode(Queue* queue);
+Queue* removeNode(Queue* queue) {
+    // 임시로 저장할 노드
+    queueData* temp;
+    // underflow 예외 처리
+    if(isEmpty(queue)) {
+        printf("큐에 데이터가 존재하지 않습니다\n");
+        return FALSE;
+    }
+    else {
+        // 현재 가리키는 노드는 삭제할 노드
+        queue->current = queue->rear->previous;
+
+        // temp에 삭제할 노드 이전(previous) 노드의 레퍼런스를 복사
+        temp = queue->rear->previous->previous;
+
+        // 삭제할 노드를 제외한 노드 연결
+        temp->next = queue->rear;
+        queue->rear->previous = temp;
+
+        // 삭제할 노드 메모리 반환
+        memset(queue->current, 0xFF, sizeof(queue->current));
+        free(queue->current);
+        queue->current->next = NULL;
+        queue->current->previous = NULL;
+    }
+    return queue;
+}
 
 // 데이터 출력(peek) : 가장 처음 데이터 노드의 데이터 출력
-Queue* peek(Queue* queue);
+void peek(Queue* queue) {
+    // 만약 데이터 노드가 존재하지 않는다면
+    if(isEmpty(queue)) {
+        printf("데이터가 존재하지 않습니다\n");
+        return;
+    }
+    // 현재 가리킬 노드는 마지막 노드의 이전 노드
+    queue->current = queue->rear->previous;
+    printf("%d\n", queue->current->number);
+}
+
 
 // 공백 여부
-_Bool isEmpty(Queue* queue);
-
-// 데이터 추가 가능 여부
-_Bool isFull(Queue* queue);
+_Bool isEmpty(Queue* queue) {
+    if(queue->front->next == queue->rear) {
+        return TRUE;
+    }
+    else {
+        return FALSE;
+    }
+}
--- queue.h
+++ queue.h
@@ -1,11 +1,14 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+// 이중 연결 리스트 노드 선언
 typedef struct queueData {
     int number;
     struct queueData *next;
+    struct queueData *previous;
 }queueData;
 
+// 이중 연결 리스트 처음/끝 노드와 순회 노드 선언
 typedef struct Queue {
     struct queueData *front;
     struct queueData *rear;
@@ -18,19 +21,16 @@ Queue* createQueue(Queue* queue);
 // 큐 제거 : 동적 할당한 모든 노드의 메모리를 반환
 _Bool deleteQueue(Queue* queue);
 
-// 큐 추가(enqueue) : 큐의 가장 마지막에 데이터를 추가
-Queue* addNode(Queue* queue, int number);
+// 큐 데이터 추가(enqueue) : 큐의 가장 마지막에 데이터를 추가
+Queue* addNode(Queue* queue, const int number);
 
-// 큐 제거(dequeue) : 큐의 가장 처음 데이터를 제거
+// 큐 데이터 제거(dequeue) : 큐의 가장 처음 데이터를 제거
 Queue* removeNode(Queue* queue);
 
 // 데이터 출력(peek) : 가장 처음 데이터 노드의 데이터 출력
-Queue* peek(Queue* queue);
+void peek(Queue* queue);
 
 // 공백 여부
 _Bool isEmpty(Queue* queue);
-
-// 데이터 추가 가능 여부
-_Bool isFull(Queue* queue);
 
 #endif
